faf9ae0ccd1074487273fb9ebd517c13b1073f5c
Jenni-Foued <medfouedjenni@gmail.com>
2021-04-02 20:11:17 +0100

    Task 0: creating a file using puppet

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# Creates the file holberton in /tmp
+
+file { 'holberton':
+  path    => '/tmp/holberton',
+  content => 'I love Puppet',
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0744',
+}
