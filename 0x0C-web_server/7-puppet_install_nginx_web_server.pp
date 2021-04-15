929228de799afb0f8c2b1b3cb5673f57f1d001f2
Jenni-Foued <medfouedjenni@gmail.com>
2021-04-15 12:18:41 +0100

    Task 7: automated using puppet

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,26 @@
+# Install Nginx web server and configure it
+
+package { 'nginx':
+  ensure   => present,
+  provider => 'apt'
+}
+
+file { '/var/www/html/index.html':
+  ensure  => present,
+  path    => '/var/www/html/index.html',
+  content => 'Holberton School'
+}
+
+file_line { 'Redirection':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'listen 80 ;',
+  line   => '\trewrite ^/redirect_me https://www.google.com/ permanent'
+}
+
+service { 'nginx':
+  ensure     => running,
+  enable     => true,
+  hasrestart => true,
+  require    => Package['nginx']
+}
