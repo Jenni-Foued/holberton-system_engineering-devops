846701f306105dc4cc14bea0d8bac9b78af818e0
Jenni-Foued <medfouedjenni@gmail.com>
2021-04-15 12:56:36 +0100

    Better version

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,16 +1,26 @@
 # Install Nginx web server and configure it
 
+#Updating system
+exec { 'Update and upgrade':
+  provider => shell,
+  command  => 'sudo apt-get -y update && sudo apt-get upgrade'
+}
+
+#Installing nginx
 package { 'nginx':
-  ensure   => present,
-  provider => 'apt'
+  ensure  => 'installed',
+  require => Exec['Update and upgrade']
 }
 
-file { '/var/www/html/index.html':
+#Creating index file
+file { 'index.nginx-debian.html':
   ensure  => present,
-  path    => '/var/www/html/index.html',
-  content => 'Holberton School'
+  path    => '/var/www/html/index.nginx-debian.html',
+  content => 'Holberton School',
+  require => Package['nginx']
 }
 
+#Defining redirection
 file_line { 'Redirection':
   ensure => 'present',
   path   => '/etc/nginx/sites-available/default',
@@ -18,10 +28,9 @@ file_line { 'Redirection':
   line   => '	    rewrite ^/redirect_me https://www.google.com/ permanent'
 }
 
-service { 'nginx':
-  ensure     => running,
-  enable     => true,
-  hasrestart => true,
-  require    => Package['nginx']
-  subscribe  => File_line['Redirection']
+#Restarting nginx
+exec { 'nginx restart':
+  provider => shell,
+  command  => 'sudo service nginx restart && sudo service nginx reload',
+  require  = File_line['Redirection']
 }
