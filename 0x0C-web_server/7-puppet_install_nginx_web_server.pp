3dec8d528a2aff36d423ac622517950b12075875
Jenni-Foued <medfouedjenni@gmail.com>
2021-04-15 13:05:34 +0100

    Fixed at last

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,4 +1,4 @@
-  # Script that setup a nginx web server on our server + redirection.
+# Install nginx web server
 
 package { 'nginx':
   ensure   => present,
@@ -12,12 +12,12 @@ file { '/var/www/html/index.html':
   content => 'Holberton School'
 }
 
-# Redirect to fabulous Rick Astley page
-file_line { 'Rick Astley showtime':
+# Redirection
+file_line { 'Redirection':
   ensure => 'present',
   path   => '/etc/nginx/sites-available/default',
   after  => 'listen 80 default_server;',
-  line   => '        rewrite ^/redirect_me https://www.youtube.com/watch?v=dQw4w9WgXcQ permanent;'
+  line   => '        rewrite ^/redirect_me https://www.youtube.com/watch?v=XtQAuSlhhJI permanent;'
 }
 
 service { 'nginx':
@@ -25,5 +25,5 @@ service { 'nginx':
   enable     => true,
   hasrestart => true,
   require    => Package['nginx'],
-  subscribe  => File_line['Rick Astley showtime']
+  subscribe  => File_line['Redirection']
 }
