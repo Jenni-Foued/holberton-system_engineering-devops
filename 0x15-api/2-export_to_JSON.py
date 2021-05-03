948ab64af10a505f570a6ac04582ed34f83d23fc
Jenni-Foued <medfouedjenni@gmail.com>
2021-05-03 23:32:39 +0100

    Initial commit

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Uses https://jsonplaceholder.typicode.com REST API and a given employee ID to
+record all tasks that are owned by this employee in a JSON file
+"""
+
+import json
+import requests
+from sys import argv
+
+if __name__ == '__main__':
+    userId = argv[1]
+    user = requests.get("https://jsonplaceholder.typicode.com/users/{}".
+                        format(userId)).json()
+    todos = requests.get("https://jsonplaceholder.typicode.com/todos?userId={}".
+                        format(userId)).json()
+    username = user.get('username')
+    tasks = []
+    for task in todos:
+        task_dict = {}
+        task_dict["task"] = task.get('title')
+        task_dict["completed"] = task.get('completed')
+        task_dict["username"] = username
+        tasks.append(task_dict)
+    jsonobj = {}
+    jsonobj[userId] = tasks
+    with open("{}.json".format(userId), 'w') as jsonfile:
+        json.dump(jsonobj, jsonfile)
