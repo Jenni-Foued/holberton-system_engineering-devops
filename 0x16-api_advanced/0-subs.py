9d2751385dc53d3a7aafd981840d1bfb9ec0a60c
Jenni-Foued <medfouedjenni@gmail.com>
2021-05-18 12:47:19 +0100

    No redirections

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,6 +9,8 @@ def number_of_subscribers(subreddit):
     queries the Reddit API and returns the number of subscribers
     for a given subreddit
     """
+    session = requests.Session()
+    session.max_redirects = 0
     url = 'http://www.reddit.com/r/{}/about.json'.format(subreddit)
     headers = {'User-Agent': 'numberOfSubscribers/0.0.1 (by /u/Franklayn)'}
     r = requests.get(url, headers=headers).json()
