21edcb04caf8aab1ce155b5c1068940b06fd817e
Jenni-Foued <medfouedjenni@gmail.com>
2021-05-18 12:24:53 +0100

    Initial commit

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Contains one function number_of_subscribers"""
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """
+    queries the Reddit API and returns the number of subscribers
+    for a given subreddit
+    """
+    if subreddit is None:
+        return 0
+    url = 'http://www.reddit.com/r/{}/about.json'.format(subreddit)
+    headers = {'User-Agent': 'numberOfSubscribers/0.0.1 (by /u/Franklayn)'}
+    r = requests.get(url, headers=headers).json()
+    nb_subs = r.get("data", {}).get("subscribers", 0)
+    return nb_subs
