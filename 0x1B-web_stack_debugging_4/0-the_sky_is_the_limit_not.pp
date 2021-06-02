b0b09d5aea572527f774c38a327ed420cda023b6
Jenni-Foued <medfouedjenni@gmail.com>
2021-06-02 16:49:07 +0100

    Increased file limit

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,6 @@
 # increases the open file limit for nginx
 exec { 'increase file limit':
   onlyif  => 'test -e /etc/default/nginx',
-  command => 'sed -i "5s/15/2000/g" /etc/default/nginx; service nginx restart',
+  command => 'sed -i "5s/15/10000/g" /etc/default/nginx; service nginx restart',
   provide => shell,
 }
