7c3d2d0d93b9a62c14329f827f2dda089609285b
Jenni-Foued <medfouedjenni@gmail.com>
2021-06-02 16:53:17 +0100

    Fixed puppet lint

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,6 @@
 # increases the open file limit for nginx
 exec { 'increase file limit':
-  onlyif  => 'test -e /etc/default/nginx',
-  command => 'sed -i "5s/15/10000/g" /etc/default/nginx; service nginx restart',
+  onlyif   => 'test -e /etc/default/nginx',
+  command  => 'sed -i "5s/15/10000/g" /etc/default/nginx; service nginx restart',
   provider => shell,
 }
