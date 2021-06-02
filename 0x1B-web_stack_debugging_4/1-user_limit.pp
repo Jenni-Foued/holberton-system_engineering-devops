e31572c9848b5e3faeff8db9a322f8aa985480b6
Jenni-Foued <medfouedjenni@gmail.com>
2021-06-02 17:36:20 +0100

    New approach

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,6 +1,6 @@
 # increases the open file limit for the os
 exec { 'Increase open file limit':
   onlyif   => 'test -e /etc/security/limits.conf',
-  command  => 'sed -i "s/holberton hard nofile 5/holberton hard nofile 97816/g" /etc/security/limits.conf; sed -i "s/holberton soft nofile 4/holberton soft nofile 97816/g" /etc/security/limits.conf'
+  command  => 'echo "" > /etc/security/limits.conf',
   provider => shell,
 }
